99a78c9be76922f960ef803a0ea42a00275d22f4
Mathew Ross <31719422+matdross@users.noreply.github.com>
2024-01-19 09:46:49 +1000

    Time: 2154 ms (11.73%), Space: 0B (100.00%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+
+SELECT DISTINCT SalesPerson.name 
+FROM SalesPerson 
+WHERE SalesPerson.sales_id NOT IN (
+    SELECT Orders.sales_id 
+    FROM Orders 
+    JOIN Company ON Orders.com_id = Company.com_id 
+    WHERE Company.name = 'RED'
+)
